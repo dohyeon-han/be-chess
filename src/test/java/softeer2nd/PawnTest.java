9bc835b4137f94fe54a219a2a075ccc2ea1bf05f
dohyeon-han <limited.hyeon@gmail.com>
2023-07-03 17:28:00 +0900

    test: Pawn 생성 테스트

---
+++ src/test/java/softeer2nd/PawnTest.java
@@ -0,0 +1,19 @@
+package softeer2nd;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PawnTest {
+
+    @Test
+    @DisplayName("폰이 생성되어야 한다")
+    public void createWhite() {
+        Pawn whitePawn = new Pawn("white");
+        assertThat(whitePawn.getColor()).isEqualTo("white");
+
+        Pawn blackPawn = new Pawn("black");
+        assertThat(blackPawn.getColor()).isEqualTo("black");
+    }
+}
