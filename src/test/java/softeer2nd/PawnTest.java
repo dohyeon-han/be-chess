c4616297ce7845696eeb319d2ae40eff64c27d97
dohyeon-han <limited.hyeon@gmail.com>
2023-07-03 18:00:08 +0900

    test: Pawn 기본 생성자 테스트

--- src/test/java/softeer2nd/PawnTest.java
+++ src/test/java/softeer2nd/PawnTest.java
@@ -17,6 +17,13 @@ class PawnTest {
         verifyPawn(BLACK);
     }
 
+    @Test
+    @DisplayName("생성자에 인자가 없으면 흰 폰이 생성된다.")
+    public void create_기본생성자() {
+        Pawn pawn = new Pawn();
+        assertThat(pawn.getColor()).isEqualTo("white");
+    }
+
     public void verifyPawn(final String color){
         Pawn pawn = new Pawn(color);
         assertThat(pawn.getColor()).isEqualTo(color);
