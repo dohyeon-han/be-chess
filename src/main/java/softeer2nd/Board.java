69aa85d394166cc75e909fe8e741a5279f6c24c2
dohyeon-han <limited.hyeon@gmail.com>
2023-07-04 10:19:25 +0900

    feat: Board 구현
    Pawn 삽입, 기물 개수, Pawn 위치 함수 구현

---
+++ src/main/java/softeer2nd/Board.java
@@ -0,0 +1,22 @@
+package softeer2nd;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Board {
+
+    private final List<Pawn> pieces = new ArrayList<>();
+
+    public void add(Pawn pawn){
+        pieces.add(pawn);
+    }
+
+    public int size() {
+        return pieces.size();
+    }
+
+    public Pawn findPawn(int idx) {
+        return pieces.get(idx);
+    }
+}
