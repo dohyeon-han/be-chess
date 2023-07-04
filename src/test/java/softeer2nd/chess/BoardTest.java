5a9d4f3a8a4e22b0d34696a3807909d6093afa1c
dohyeon-han <limited.hyeon@gmail.com>
2023-07-04 14:33:11 +0900

    test: 체스판 초기화 출력

--- src/test/java/softeer2nd/chess/BoardTest.java
+++ src/test/java/softeer2nd/chess/BoardTest.java
@@ -1,18 +1,40 @@
 package softeer2nd.chess;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class BoardTest {
+    Board board;
+
+    @BeforeEach
+    public void setUp() {
+        board = new Board();
+        board.initialize();
+    }
 
     @Test
     @DisplayName("폰을 출력한다.")
     public void initialize() {
-        Board board = new Board();
-        board.initialize();
         assertThat(board.getWhitePawnsResult()).isEqualTo("pppppppp");
         assertThat(board.getBlackPawnsResult()).isEqualTo("PPPPPPPP");
+    }
+
+    @Test
+    @DisplayName("초기화한 체스판을 출력한다.")
+    public void printInitialize() {
+        final String init = "........\npppppppp\n........\n........\n........\n........\nPPPPPPPP\n........";
+        final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStreamCaptor));
+
+        board.print();
+        assertThat(outputStreamCaptor.toString().trim()).isEqualTo(init);
+
+        System.setOut(new PrintStream(System.out));
     }
 }
