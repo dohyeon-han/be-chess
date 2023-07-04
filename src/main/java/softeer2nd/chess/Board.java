6ef07a33d83cabf45fba0997c34c4e3d32846365
dohyeon-han <limited.hyeon@gmail.com>
2023-07-04 12:06:12 +0900

    feat: Pawn을 Board에 초기화
    폰 초기화 및 초기 폰 결과 출력 구현

--- src/main/java/softeer2nd/chess/Board.java
+++ src/main/java/softeer2nd/chess/Board.java
@@ -7,6 +7,18 @@ import java.util.List;
 public class Board {
 
     private final List<Pawn> pieces = new ArrayList<>();
+    private final List<StringBuilder> board = new ArrayList<>();
+
+    public void initialize() {
+        board.add(new StringBuilder("........"));
+        board.add(new StringBuilder("PPPPPPPP"));
+        board.add(new StringBuilder("........"));
+        board.add(new StringBuilder("........"));
+        board.add(new StringBuilder("........"));
+        board.add(new StringBuilder("........"));
+        board.add(new StringBuilder("pppppppp"));
+        board.add(new StringBuilder("........"));
+    }
 
     public void add(Pawn pawn){
         pieces.add(pawn);
@@ -18,5 +30,13 @@ public class Board {
 
     public Pawn findPawn(int idx) {
         return pieces.get(idx);
+    }
+
+    public String getWhitePawnsResult() {
+        return this.board.get(6).toString();
+    }
+
+    public String getBlackPawnsResult() {
+        return this.board.get(1).toString();
     }
 }
