5c5d1711fdb6e5373f16f8f8e57e14eb0c10aa31
dohyeon-han <limited.hyeon@gmail.com>
2023-07-04 13:47:43 +0900

    refactor: pawn list를  black과 white로 분리

--- src/main/java/softeer2nd/chess/Board.java
+++ src/main/java/softeer2nd/chess/Board.java
@@ -2,41 +2,37 @@ package softeer2nd.chess;
 
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.stream.Collectors;
 
 public class Board {
 
-    private final List<Pawn> pieces = new ArrayList<>();
-    private final List<StringBuilder> board = new ArrayList<>();
+    private final List<Pawn> whitePawns = new ArrayList<>();
+    private final List<Pawn> blackPawns = new ArrayList<>();
+    private final Pawn[][] board = new Pawn[8][8];
 
     public void initialize() {
-        board.add(new StringBuilder("........"));
-        board.add(new StringBuilder("PPPPPPPP"));
-        board.add(new StringBuilder("........"));
-        board.add(new StringBuilder("........"));
-        board.add(new StringBuilder("........"));
-        board.add(new StringBuilder("........"));
-        board.add(new StringBuilder("pppppppp"));
-        board.add(new StringBuilder("........"));
-    }
+        for (int i = 0; i < 8; i++) {
+            Pawn white = new Pawn();
+            whitePawns.add(white);
+            board[1][i] = white;
 
-    public void add(Pawn pawn){
-        pieces.add(pawn);
-    }
-
-    public int size() {
-        return pieces.size();
-    }
-
-    public Pawn findPawn(int idx) {
-        return pieces.get(idx);
+            Pawn black = new Pawn(Pawn.BLACK_COLOR, Pawn.BLACK_REPRESENTATION);
+            blackPawns.add(black);
+            board[6][i] = black;
+        }
     }
 
     public String getWhitePawnsResult() {
-        return this.board.get(6).toString();
+        return whitePawns.stream()
+                .map(Pawn::getRepresentation)
+                .map(String::valueOf)
+                .collect(Collectors.joining());
     }
 
     public String getBlackPawnsResult() {
-        return this.board.get(1).toString();
+        return blackPawns.stream()
+                .map(Pawn::getRepresentation)
+                .map(String::valueOf)
+                .collect(Collectors.joining());
     }
 }
