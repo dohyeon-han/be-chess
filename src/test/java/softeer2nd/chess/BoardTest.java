edbe6713e6bf9a19b4522860854131de77b27c8c
dohyeon-han <limited.hyeon@gmail.com>
2023-07-04 13:42:17 +0900

    test: pawn 추가 테스트 삭제

--- src/test/java/softeer2nd/chess/BoardTest.java
+++ src/test/java/softeer2nd/chess/BoardTest.java
@@ -8,33 +8,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class BoardTest {
 
     @Test
-    @DisplayName("흰 폰과 검정 폰이 체스판에 추가된다.")
-    public void create() {
-        Board board = new Board();
-
-        Pawn white = addPawn(board, Pawn.WHITE_COLOR, Pawn.WHITE_REPRESENTATION);
-        verifyBoard(board, white, 1, 0);
-
-        Pawn black = addPawn(board, Pawn.BLACK_COLOR, Pawn.BLACK_REPRESENTATION);
-        verifyBoard(board, black, 2, 1);
-    }
-
-    @Test
     public void initialize() {
         Board board = new Board();
         board.initialize();
         assertThat(board.getWhitePawnsResult()).isEqualTo("pppppppp");
         assertThat(board.getBlackPawnsResult()).isEqualTo("PPPPPPPP");
-    }
-
-    private Pawn addPawn(Board board, String color, char representation) {
-        Pawn pawn = new Pawn(color, representation);
-        board.add(pawn);
-        return pawn;
-    }
-
-    private void verifyBoard(Board board, Pawn pawn, int size, int idx) {
-        assertThat(size).isEqualTo(board.size());
-        assertThat(pawn).isEqualTo(board.findPawn(idx));
     }
 }
