7c220adfbe56966cafe9d87591fa1e6dd7e40b44
dohyeon-han <limited.hyeon@gmail.com>
2023-07-04 17:05:12 +0900

    chore: 체스 시작/종료 구문 추가

--- src/main/java/softeer2nd/chess/Main.java
+++ src/main/java/softeer2nd/chess/Main.java
@@ -5,15 +5,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Board board = new Board();
-        board.initialize();
         boolean playing = false;
+
         Scanner sc = new Scanner(System.in);
+
         while (sc.hasNext()) {
             String input = sc.next();
             if (input.equals("start")) {
                 playing = true;
+                System.out.println("체스를 시작합니다.");
+                board.initialize();
                 board.print();
             } else if (input.equals("end")) {
+                System.out.println("체스를 종료합니다.");
                 break;
             } else if(playing) {
                 board.print();
