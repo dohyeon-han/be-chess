5e9cb0be4b774377d65c8237c52c2bc622dc23e4
dohyeon-han <limited.hyeon@gmail.com>
2023-07-05 09:57:15 +0900

    test: 문자열에 줄바꿈 추가 메소드 구현

---
+++ src/test/java/softeer2nd/chess/StringUtilsTest.java
@@ -0,0 +1,16 @@
+package softeer2nd.chess;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class StringUtilsTest {
+
+    @Test
+    @DisplayName("문자열을 줄바꿈한다.")
+    public void appendNewLine() {
+        String str = "test";
+        assertThat(StringUtils.appendNewLine(str)).isEqualTo("test\n");
+    }
+}
