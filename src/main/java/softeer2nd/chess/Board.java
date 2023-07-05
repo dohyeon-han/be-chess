440abb8db2eaf70ce1fe42ed5e49df13838e3424
dohyeon-han <limited.hyeon@gmail.com>
2023-07-05 17:31:53 +0900

    feat: 2차원 배열을 2차원 List로 변경 및 초기화
    빈 자리에는 Blank piece를 추가

--- src/main/java/softeer2nd/chess/Board.java
+++ src/main/java/softeer2nd/chess/Board.java
@@ -3,51 +3,62 @@ package softeer2nd.chess;
 import softeer2nd.chess.util.PieceUtils;
 import softeer2nd.chess.util.StringUtils;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class Board {
 
-    private final Piece[][] board = new Piece[8][8];
+    public final int BOARD_LENGTH = 8;
+    private final List<List<Piece>> board = new ArrayList<>(BOARD_LENGTH);
 
     public void initialize() {
+        for (int i = 0; i < BOARD_LENGTH; i++) {
+            List<Piece> row = new ArrayList<>(BOARD_LENGTH);
+            board.add(row);
+        }
         initializeByColor(PieceUtils.Color.WHITE);
         initializeByColor(PieceUtils.Color.BLACK);
+        initializeBlank();
     }
+
 
     private void initializeByColor(PieceUtils.Color color) {
         int pawnRow = color.equals(PieceUtils.Color.BLACK) ? 1 : 6;
         int otherRow = color.equals(PieceUtils.Color.BLACK) ? 0 : 7;
 
         for (int i = 0; i < 8; i++) {
-            board[pawnRow][i] = Piece.createPiece(color, PieceUtils.Type.PAWN);
+            board.get(pawnRow).add(Piece.createPiece(color, PieceUtils.Type.PAWN));
         }
 
-        board[otherRow][0] = Piece.createPiece(color, PieceUtils.Type.ROOK);
-        board[otherRow][7] = Piece.createPiece(color, PieceUtils.Type.ROOK);
+        board.get(otherRow).add(Piece.createPiece(color, PieceUtils.Type.ROOK));
+        board.get(otherRow).add(Piece.createPiece(color, PieceUtils.Type.KNIGHT));
+        board.get(otherRow).add(Piece.createPiece(color, PieceUtils.Type.BISHOP));
+        board.get(otherRow).add(Piece.createPiece(color, PieceUtils.Type.QUEEN));
+        board.get(otherRow).add(Piece.createPiece(color, PieceUtils.Type.KING));
+        board.get(otherRow).add(Piece.createPiece(color, PieceUtils.Type.BISHOP));
+        board.get(otherRow).add(Piece.createPiece(color, PieceUtils.Type.KNIGHT));
+        board.get(otherRow).add(Piece.createPiece(color, PieceUtils.Type.ROOK));
+    }
 
-        board[otherRow][1] = Piece.createPiece(color, PieceUtils.Type.KNIGHT);
-        board[otherRow][6] = Piece.createPiece(color, PieceUtils.Type.KNIGHT);
-
-        board[otherRow][2] = Piece.createPiece(color, PieceUtils.Type.BISHOP);
-        board[otherRow][5] = Piece.createPiece(color, PieceUtils.Type.BISHOP);
-
-        board[otherRow][3] = Piece.createPiece(color, PieceUtils.Type.QUEEN);
-
-        board[otherRow][4] = Piece.createPiece(color, PieceUtils.Type.KING);
+    private void initializeBlank() {
+        for (int i = 2; i < 6; i++) {
+            for (int j = 0; j < BOARD_LENGTH; j++) {
+                board.get(i).add(Piece.createBlank());
+            }
+        }
     }
 
     public String getWhitePawnsResult() {
-        return getRepresentationResult(board[6]);
+        return getRepresentationResult(board.get(6));
     }
 
     public String getBlackPawnsResult() {
-        return getRepresentationResult(board[1]);
+        return getRepresentationResult(board.get(1));
     }
 
-    private String getRepresentationResult(Piece[] row) {
-        return Arrays.stream(row)
-                .map(Piece::getRepresentation)
+    private String getRepresentationResult(List<Piece> row) {
+        return row.stream().map(Piece::getRepresentation)
                 .map(String::valueOf)
                 .collect(Collectors.joining());
     }
@@ -56,25 +67,22 @@ public class Board {
         System.out.print(showBoard());
     }
 
-    public int pieceCount() {
-        int count = 0;
-        for (Piece[] row : board) {
-            for (Piece piece : row) {
-                if (piece == null) continue;
-                count++;
-            }
-        }
-        return count;
+    public long pieceCount() {
+        return this.board.stream()
+                .flatMap(List::stream)
+                .filter(piece -> !piece.getType().equals(PieceUtils.Type.NO_PIECE))
+                .count();
+
     }
 
     public String showBoard() {
         StringBuilder builder = new StringBuilder();
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
-                if (board[i][j] == null) {
+                if (board.get(i).get(j).getType().equals(PieceUtils.Type.NO_PIECE)) {
                     builder.append('.');
                 } else {
-                    builder.append(board[i][j].getRepresentation());
+                    builder.append(board.get(i).get(j).getRepresentation());
                 }
             }
             builder.append(StringUtils.NEWLINE);
