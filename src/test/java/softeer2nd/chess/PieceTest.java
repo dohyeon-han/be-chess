96e21010941a8f1099b8ed9d092efbff50ff6d21
dohyeon-han <limited.hyeon@gmail.com>
2023-07-05 11:43:42 +0900

    test: 팩토리 메소드로 기물 생성

--- src/test/java/softeer2nd/chess/PieceTest.java
+++ src/test/java/softeer2nd/chess/PieceTest.java
@@ -9,10 +9,25 @@ import static org.assertj.core.api.Assertions.assertThat;
 class PieceTest {
 
     @Test
-    @DisplayName("폰이 생성되어야 한다")
-    public void createWhite() {
-        verifyPawn(Piece.createWhitePawn(), PieceUtils.Color.WHITE, PieceUtils.Type.PAWN);
-        verifyPawn(Piece.createBlackPawn(), PieceUtils.Color.BLACK, PieceUtils.Type.PAWN);
+    @DisplayName("기물이 생성되어야 한다")
+    public void createPiece() {
+        verifyPawn(Piece.createPiece(PieceUtils.Color.WHITE, PieceUtils.Type.PAWN), PieceUtils.Color.WHITE, PieceUtils.Type.PAWN);
+        verifyPawn(Piece.createPiece(PieceUtils.Color.BLACK, PieceUtils.Type.PAWN), PieceUtils.Color.BLACK, PieceUtils.Type.PAWN);
+
+        verifyPawn(Piece.createPiece(PieceUtils.Color.WHITE, PieceUtils.Type.KNIGHT), PieceUtils.Color.WHITE, PieceUtils.Type.KNIGHT);
+        verifyPawn(Piece.createPiece(PieceUtils.Color.BLACK, PieceUtils.Type.KNIGHT), PieceUtils.Color.BLACK, PieceUtils.Type.KNIGHT);
+
+        verifyPawn(Piece.createPiece(PieceUtils.Color.WHITE, PieceUtils.Type.ROOK), PieceUtils.Color.WHITE, PieceUtils.Type.ROOK);
+        verifyPawn(Piece.createPiece(PieceUtils.Color.BLACK, PieceUtils.Type.ROOK), PieceUtils.Color.BLACK, PieceUtils.Type.ROOK);
+
+        verifyPawn(Piece.createPiece(PieceUtils.Color.WHITE, PieceUtils.Type.KING), PieceUtils.Color.WHITE, PieceUtils.Type.KING);
+        verifyPawn(Piece.createPiece(PieceUtils.Color.BLACK, PieceUtils.Type.KING), PieceUtils.Color.BLACK, PieceUtils.Type.KING);
+
+        verifyPawn(Piece.createPiece(PieceUtils.Color.WHITE, PieceUtils.Type.BISHOP), PieceUtils.Color.WHITE, PieceUtils.Type.BISHOP);
+        verifyPawn(Piece.createPiece(PieceUtils.Color.BLACK, PieceUtils.Type.BISHOP), PieceUtils.Color.BLACK, PieceUtils.Type.BISHOP);
+
+        verifyPawn(Piece.createPiece(PieceUtils.Color.WHITE, PieceUtils.Type.QUEEN), PieceUtils.Color.WHITE, PieceUtils.Type.QUEEN);
+        verifyPawn(Piece.createPiece(PieceUtils.Color.BLACK, PieceUtils.Type.QUEEN), PieceUtils.Color.BLACK, PieceUtils.Type.QUEEN);
     }
 
     private void verifyPawn(Piece piece, final PieceUtils.Color color, final PieceUtils.Type type){
