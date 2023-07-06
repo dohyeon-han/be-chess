fcc7104fb79f8aee451b7ccd37ef5e354c99dac7
dohyeon-han <limited.hyeon@gmail.com>
2023-07-06 17:09:39 +0900

    feat: 기물 이동 명령 구현

--- src/main/java/softeer2nd/chess/Main.java
+++ src/main/java/softeer2nd/chess/Main.java
@@ -10,7 +10,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         while (sc.hasNext()) {
-            String input = sc.next();
+            String input = sc.nextLine();
             if (input.equals("start")) {
                 playing = true;
                 System.out.println("체스를 시작합니다.");
@@ -20,8 +20,22 @@ public class Main {
                 System.out.println("체스를 종료합니다.");
                 break;
             } else if (playing) {
+                String[] tokens = input.split(" ");
+                command(board, tokens);
                 board.print();
             }
+        }
+    }
+
+    private static void command(Board board, String[] tokens) {
+        try {
+            if (tokens.length == 3) {
+                if (tokens[0].equals("move")) {
+                    board.move(tokens[1], tokens[2]);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("잘못된 명령입니다.");
         }
     }
 }
