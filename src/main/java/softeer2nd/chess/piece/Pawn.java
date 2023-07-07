42f87e83c51e026e2ee51021ea70883b70cd98db
dohyeon-han <limited.hyeon@gmail.com>
2023-07-07 13:39:32 +0900

    feat: 폰 이동 검증
    시작 줄에서 두 칸 이동은 미구현

--- src/main/java/softeer2nd/chess/piece/Pawn.java
+++ src/main/java/softeer2nd/chess/piece/Pawn.java
@@ -1,5 +1,6 @@
 package softeer2nd.chess.piece;
 
+import softeer2nd.chess.Position;
 import softeer2nd.chess.util.PieceUtils.Color;
 import softeer2nd.chess.util.PieceUtils.Type;
 
@@ -16,5 +17,27 @@ public class Pawn extends Piece {
 
     public static Pawn createBlackPawn() {
         return new Pawn(Color.BLACK, Type.PAWN);
+    }
+
+    public void verifyMovePosition(Position source, Position destination) {
+        int yDist = destination.getY() - source.getY();
+        int xDist = destination.getX() - source.getX();
+        if(this.color.equals(Color.WHITE)) {
+            verifyWhiteMovePosition(yDist, xDist);
+        } else {
+            verifyBlackMovePosition(yDist, xDist);
+        }
+    }
+
+    private void verifyWhiteMovePosition(int yDist, int xDist) {
+        if(yDist != 1 || Math.abs(xDist) > 1) {
+            throw new IllegalArgumentException("적절하지 않은 이동입니다.");
+        }
+    }
+
+    private void verifyBlackMovePosition(int yDist, int xDist) {
+        if(yDist != -1 || Math.abs(xDist) > 1) {
+            throw new IllegalArgumentException("적절하지 않은 이동입니다.");
+        }
     }
 }
---
+++ src/test/java/softeer2nd/chess/piece/PawnTest.java
@@ -0,0 +1,90 @@
+package softeer2nd.chess.piece;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import softeer2nd.chess.Position;
+
+import static org.mockito.Mockito.*;
+
+public class PawnTest {
+
+    @Test
+    @DisplayName("흰 폰은 아래로 1칸 이동할 수 있다.")
+    public void moveDownWhitePawn() {
+        //given
+        Position source = new Position("g7");
+        Position target = new Position("g6");
+        Pawn pawn = spy(Pawn.createWhitePawn());
+
+        //when, then
+        verifyMove(pawn, source, target);
+    }
+
+    @Test
+    @DisplayName("흰 폰은 왼쪽 대각선 아래로 이동할 수 있다.")
+    public void moveDownLeftWhitePawn() {
+        //given
+        Position source = new Position("g7");
+        Position target = new Position("f6");
+        Pawn pawn = spy(Pawn.createWhitePawn());
+
+        //when, then
+        verifyMove(pawn, source, target);
+    }
+
+    @Test
+    @DisplayName("흰 폰은 오른쪽 대각선 아래로 이동할 수 있다.")
+    public void moveDownRightWhitePawn() {
+        //given
+        Position source = new Position("f7");
+        Position target = new Position("g6");
+        Pawn pawn = spy(Pawn.createWhitePawn());
+
+        //when, then
+        verifyMove(pawn, source, target);
+    }
+
+    @Test
+    @DisplayName("검정 폰은 위로 1칸 이동할 수 있다.")
+    public void moveUpWhitePawn() {
+        //given
+        Position source = new Position("a2");
+        Position target = new Position("a3");
+        Pawn pawn = spy(Pawn.createBlackPawn());
+
+        //when, then
+        verifyMove(pawn, source, target);
+    }
+
+    @Test
+    @DisplayName("검정 폰은 왼쪽 대각선 위로 이동할 수 있다.")
+    public void moveUpLeftWhitePawn() {
+        //given
+        Position source = new Position("b2");
+        Position target = new Position("a3");
+        Pawn pawn = spy(Pawn.createBlackPawn());
+
+        //when, then
+        verifyMove(pawn, source, target);
+    }
+
+    @Test
+    @DisplayName("검정 폰은 오른쪽 대각선 위로 이동할 수 있다.")
+    public void moveUpRightWhitePawn() {
+        //given
+        Position source = new Position("a2");
+        Position target = new Position("b3");
+        Pawn pawn = spy(Pawn.createBlackPawn());
+
+        //when, then
+        verifyMove(pawn, source, target);
+    }
+
+    private void verifyMove(Pawn pawn, Position source, Position target) {
+        //when
+        pawn.verifyMovePosition(source, target);
+
+        //then
+        verify(pawn, times(1)).verifyMovePosition(source, target);
+    }
+}
